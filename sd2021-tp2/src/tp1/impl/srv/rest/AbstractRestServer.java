a353a61545c4db1d87425a89395b8d599241c8fe
hemareso <hemareso@gmail.com>
2021-06-04 11:57:47 +0100

    discovery file

--- sd2021-tp2/src/tp1/impl/srv/rest/AbstractRestServer.java
+++ sd2021-tp2/src/tp1/impl/srv/rest/AbstractRestServer.java
@@ -1,7 +1,11 @@
 package tp1.impl.srv.rest;
 
 import java.net.URI;
+import java.security.NoSuchAlgorithmException;
 import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -9,41 +13,50 @@ import org.glassfish.jersey.server.ResourceConfig;
 import tp1.impl.discovery.Discovery;
 import tp1.impl.srv.Domain;
 import tp1.impl.utils.IP;
+import tp1.impl.utils.InsecureHostnameVerifier;
 
 public abstract class AbstractRestServer {
-	
-	protected static String SERVER_BASE_URI = "http://%s:%s/rest";
+
+	protected static String SERVER_BASE_URI = "https://%s:%s/rest";
 
 	final int port;
 	final String service;
 	final private Logger Log;
-	
+
 	protected AbstractRestServer(Logger log, String service, int port) {
 		this.service = service;
 		this.port = port;
 		this.Log = log;
 	}
 
-
 	protected void start() {
-		String ip = IP.hostAddress();
-		String serverURI = String.format(SERVER_BASE_URI, ip, port);
-		String FullServiceName = String.format("%s:%s", Domain.get(), service);
-		
-		ResourceConfig config = new ResourceConfig();
-		
-		registerResources( config );
-		
-		JdkHttpServerFactory.createHttpServer( URI.create(serverURI.replace(ip, "0.0.0.0")), config);
+		try {
+			String ip = IP.hostAddress();
+			String serverURI = String.format(SERVER_BASE_URI, ip, port);
+			String FullServiceName = String.format("%s:%s", Domain.get(), service);
 
-		Log.info(String.format("%s Server ready @ %s\n",  service, serverURI));
-		
-		Discovery.getInstance().announce(FullServiceName, serverURI);
+			// This allows client code executed by this server to ignore hostname
+			// verification
+			HttpsURLConnection.setDefaultHostnameVerifier(new InsecureHostnameVerifier());
 
+			ResourceConfig config = new ResourceConfig();
+
+			registerResources(config);
+
+			JdkHttpServerFactory.createHttpServer(URI.create(serverURI.replace(ip, "0.0.0.0")), config,
+					SSLContext.getDefault());
+
+			Log.info(String.format("%s Server ready @ %s\n", service, serverURI));
+
+			Discovery.getInstance().announce(FullServiceName, serverURI);
+
+		} catch (Exception e) {
+			Log.severe(e.getMessage());
+		}
 	}
-	
-	abstract void registerResources( ResourceConfig config );
-	
+
+	abstract void registerResources(ResourceConfig config);
+
 	static {
 		System.setProperty("java.net.preferIPv4Stack", "true");
 		System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s");
--- sd2021-tp2/src/tp1/impl/srv/soap/SpreadsheetsSoapServer.java
+++ sd2021-tp2/src/tp1/impl/srv/soap/SpreadsheetsSoapServer.java
@@ -1,20 +1,28 @@
 package tp1.impl.srv.soap;
 
-
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
 
 import jakarta.xml.ws.Endpoint;
 import tp1.impl.discovery.Discovery;
 import tp1.impl.srv.Domain;
 import tp1.impl.utils.IP;
-
+import tp1.impl.utils.InsecureHostnameVerifier;
 
 public class SpreadsheetsSoapServer {
 
 	public static final int PORT = 14567;
 	public static final String SERVICE_NAME = "sheets";
 	public static String SERVER_BASE_URI = "http://%s:%s/soap";
+	public static final String SOAP_SHEETS_PATH = "/soap/sheets";
 
 	private static Logger Log = Logger.getLogger(SpreadsheetsSoapServer.class.getName());
 
@@ -32,9 +40,28 @@ public class SpreadsheetsSoapServer {
 		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
 		String FullServiceName = String.format("%s:%s", Domain.get(), SERVICE_NAME);
 
-		Endpoint.publish(serverURI, new SoapSpreadsheetsWebService());
+		// This allows client code executed by this server to ignore hostname
+		// verification
+		HttpsURLConnection.setDefaultHostnameVerifier(new InsecureHostnameVerifier());
+
+		// Create an https configurator to define the SSL/TLS context
+		HttpsConfigurator configurator = new HttpsConfigurator(SSLContext.getDefault());
+
+		HttpsServer server = HttpsServer.create(new InetSocketAddress(ip, PORT), 0);
+
+		server.setHttpsConfigurator(configurator);
+
+		server.setExecutor(Executors.newCachedThreadPool());
+
+		Endpoint soapSpreadsheetsEndpoint = Endpoint.create(new SoapSpreadsheetsWebService());
+
+		//Endpoint.publish(serverURI, new SoapSpreadsheetsWebService());
 
 		Discovery.getInstance().announce(FullServiceName, serverURI);
+
+		soapSpreadsheetsEndpoint.publish(server.createContext(SOAP_SHEETS_PATH));
+
+		server.start();
 
 		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
 
